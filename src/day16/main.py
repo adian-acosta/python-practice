003cfe100098579a884b04c99a92dcd1a2602314
Cometo <adian_acosta44@outlook.com>
2025-11-08 19:34:50 -0600

    add .gitignore file and day 16 files

--- src/day16/main.py
+++ src/day16/main.py
@@ -1,3 +1,33 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+running = True
+
+while running:
+    order = input(f"What would you like? ({menu.get_items()}): ").lower().strip()
+
+    if order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+        print()
+        continue
+    elif order == "off":
+        print('Turning off Q_Q...')
+        running = False
+        continue
+
+    order = menu.find_drink(order)
+    if order is not None:
+        if not coffee_maker.is_resource_sufficient(order):
+            continue
+        if not money_machine.make_payment(order.cost):
+            continue
+
+        coffee_maker.make_coffee(order)
+
+    print()
